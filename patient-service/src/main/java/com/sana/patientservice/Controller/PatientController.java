fd81f0e7d8d91ff2320595d37da9550e9a7d1661
Sanakkiyan <ssanakkiyan63@gmail.com>
2025-07-06 00:13:25 +0530

    Dockerize it all service.
    Signed-off-by: Sanakkiyan <ssanakkiyan63@gmail.com>

--- patient-service/src/main/java/com/sana/patientservice/Controller/PatientController.java
+++ patient-service/src/main/java/com/sana/patientservice/Controller/PatientController.java
@@ -8,7 +8,6 @@ import com.sana.patientservice.Service.PatientService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
